3a1a40e39722ead36749ce8f8386addda1c9f549
Craig McGowan <mrx6@cdc.gov>
2016-10-25 16:37:19 -0400

    Reset import code to only pull a particular week of submissions

--- Unweighted_Average/R/import_forecasts.R
+++ Unweighted_Average/R/import_forecasts.R
@@ -1,21 +1,21 @@
 require(dplyr)
 
 
-import_forecasts <- function(this_dir){#}, forecast_wk) {
+import_forecasts <- function(this_dir, forecast_wk) {
   file_names <- list.files(this_dir, recursive=T)
   if (length(file_names) == 0) stop("No files found; check directory.")
   
-  # # Only take files of week of interest
-  # these_files <- grep(paste0("EW", forecast_wk), file_names, value = TRUE)
-  these_files <- file_names
+  # Only take files of week of interest
+  these_files <- grep(paste0("EW", forecast_wk), file_names, value = TRUE)
+  # these_files <- file_names
   forecast_data <- data.frame()
   for (this_file in these_files) {
     if (grepl("\\.csv", this_file) & !(grepl("Target", this_file))) {
       this_sub <- read.csv(paste0(this_dir, this_file),
-                            stringsAsFactors = FALSE) %>%
-                          mutate(forecast_wk = as.numeric(gsub("EW","",regmatches(this_file,
-                                                  regexpr("(?:EW)[0-9]{2}", this_file)))),
-                                 team = gsub("\\/.*$", "", this_file))
+                            stringsAsFactors = FALSE) #%>%
+                          # mutate(forecast_wk = as.numeric(gsub("EW","",regmatches(this_file,
+                          #                         regexpr("(?:EW)[0-9]{2}", this_file)))),
+                          #        team = gsub("\\/.*$", "", this_file))
       forecast_data <- rbind(forecast_data, this_sub)
     }
   }
