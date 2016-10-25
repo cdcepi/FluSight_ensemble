44c9e15c0cb8250df1b33ca5ed80021af4661825
Craig McGowan <mrx6@cdc.gov>
2016-10-25 16:37:31 -0400

    Move code to helper functions

--- Unweighted_Average/R/Unweighted_Average.R
+++ Unweighted_Average/R/Unweighted_Average.R
@@ -10,7 +10,7 @@ dir_2016 <- "Data/2016-2017/"
 
 # Get forecast data -------------------------------------------------
 source("Unweighted_Average/R/import_forecasts.R")
-forecast_data <- import_forecasts(dir_2015)
+forecast_data <- import_forecasts(dir_2015, forecast_wk = 12)
 
 ##### Create equal weight ensemble forecast for 2015/2016 season #####
 weights <- data.frame(team = unique(forecast_data$team),
@@ -18,41 +18,14 @@ weights <- data.frame(team = unique(forecast_data$team),
                                    length(unique(forecast_data$team))))
 
 #Create shell to hold results
-ensemble.results <- NULL
+source("Unweighted_Average/R/create_ensemble.R")
 
-for(this_wk in unique(forecast_data$forecast_wk)){
-  date.results <- NULL
-  for(this_location in unique(forecast_data$location)){
-    for(this_target in unique(forecast_data$target)){
-      
-      #Filter data for particular date/location/target combination
-      these_data <- filter(forecast_data, 
-                           forecast_wk == this_wk & location == this_location & 
-                               target == this_target & type == "Bin" & !is.na(value))
-      
-      for(this_bin in unique(these_data$bin_start_incl)){
-        
-        #Determine which teams submitted predictions and normalize relevant weights
-        these.values <- filter(these.data, name==this.bin & !is.na(value))
-        these.weights <- filter(weights, team %in% these.values$team) %>%
-                            select(weight) %>% 
-                            as.matrix()
-        these.weights <- these.weights/sum(these.weights)
-        
-        #Create row with ensemble forecast
-        this.pred <- these.values[1,]
-        this.pred$team <- "ensemble"
-        this.pred$value <- weighted.mean(these.values$value, these.weights)
-        
-        date.results <- rbind(date.results,this.pred)
-      }
-    }
-  }
-  ensemble.results <- rbind(ensemble.results,date.results)
-}
+ensemble <- create_ensemble(forecast_data, forecast_wk = 12)
 
-#Combine with forecasts from other teams
-ensemble_data <- rbind(forecast_data,ensemble.results)
+wk_results <- NULL
+
+
+
 
 #Output to csv
 write.table(ensemble_data, file="Ensemble_Team_Forecasts.csv", sep=",",
